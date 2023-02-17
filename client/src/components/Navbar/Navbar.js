b01f55b71ba4b920a5acec55ddf1d43a70836b21
Ahmed Eid <Ahmed_tech67@outlook.com>
2023-02-17 23:05:55 +0200

    Make navbr responsive

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,4 +1,6 @@
 import { FaFacebookF, FaSnapchatSquare, FaWhatsapp } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { GrFormClose } from "react-icons/gr";
 import {
   AiOutlineInstagram,
   AiOutlineTwitter,
@@ -7,8 +9,61 @@ import {
 
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import { useState } from "react";
 
 function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const openMenuHandler = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  const menu = (
+    <div className="menu-list">
+      <div className="close">
+        <GrFormClose />
+      </div>
+      <ul>
+        <li>
+          <a href="#a">
+            <img src={logo} alt="Logo" />
+          </a>
+        </li>
+        <li>
+          <a href="#b">مكتبة اطبع</a>
+        </li>
+        <li>
+          <a href="#c">تسجيل الدخول</a>
+        </li>
+        <li>
+          <a href="#d">انشاء حساب</a>
+        </li>
+        <li>
+          <a href="#e">التغليف الفاخر</a>
+        </li>
+      </ul>
+      <div className="social">
+        <a href="https://www.facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https://www.instagram.com">
+          <AiOutlineInstagram />
+        </a>
+        <a href="https://www.twitter.com">
+          <AiOutlineTwitter />
+        </a>
+        <a href="https://www.youtube.com">
+          <AiFillYoutube />
+        </a>
+        <a href="https://www.snapchat.com">
+          <FaSnapchatSquare />
+        </a>
+        <a href="https://www.whatsapp.com">
+          <FaWhatsapp />
+        </a>
+      </div>
+    </div>
+  );
   return (
     <>
       <div className="navlinks">
@@ -43,6 +98,13 @@ function Navbar() {
         <div className="navbar">
           <div className="navbar_logo">
             <img src={logo} alt="Logo" />
+          </div>
+
+          <div className="menu">
+            <p>EN</p>
+            <div onClick={openMenuHandler}>
+              {openMenu ? menu : <RxHamburgerMenu />}
+            </div>
           </div>
 
           <div className="navbar_btns">
